4ea2a109260a88695b593237d638dd8e300ece87
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-05-16 20:52:37 -0400

    Solutions

--- compile.R
+++ compile.R
@@ -1,4 +1,4 @@
 # Compiles book
 
-knitr::knit('CCPDA_book.Rnw')
-system('texi2pdf CCPDA_book.tex')
+knitr::knit('CCPDA_book.Rnw'); system('texi2pdf CCPDA_book.tex')
+knitr::knit('solutions.Rnw'); system('texi2pdf solutions.tex')
